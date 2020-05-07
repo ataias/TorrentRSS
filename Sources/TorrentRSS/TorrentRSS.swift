15adbe350ef3e5c5d3d6819f1ecc50358c83311c
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-05-07 07:21:02 -0300

    Add check if transmission successfully connected

--- Sources/TorrentRSS/TorrentRSS.swift
+++ Sources/TorrentRSS/TorrentRSS.swift
@@ -41,57 +41,88 @@ public struct TorrentRSS {
 
     }
 
+    private func getTransmissionClient() -> Transmission? {
+        let client = Transmission(
+            baseURL: serverConfig.server,
+            username: serverConfig.username,
+            password: serverConfig.password)
+
+        var cancellables = Set<AnyCancellable>()
+
+        let group = DispatchGroup()
+        group.enter()
+        print("[INFO] Connecting to client")
+        client.request(.rpcVersion)
+            .sink(receiveCompletion: { _ in }, receiveValue: { rpcVersion in
+                print("[INFO]: Successfully Connected! RPC Version: \(rpcVersion)")
+                group.leave()
+            })
+            .store(in: &cancellables)
+        let wallTimeout = DispatchWallTime.now() +
+            DispatchTimeInterval.seconds(serverConfig.secondsTimeout ?? 15)
+        let res = group.wait(wallTimeout: wallTimeout)
+
+        if res == DispatchTimeoutResult.success {
+            return client
+        } else {
+            return nil
+        }
+
+    }
+
     public func updateTransmission() throws {
 
-            print("[INFO] [\(Date())] Starting Transmission Update")
+        print("[INFO] [\(Date())] Starting Transmission Update")
 
-            let client = Transmission(
-                baseURL: serverConfig.server,
-                username: serverConfig.username,
-                password: serverConfig.password)
-
-            var cancellables = Set<AnyCancellable>()
-
-            let items = try store.getPendingDownload()
-            print("[INFO] [\(Date())] Adding \(items.count) new items to transmission")
-
-            let group = DispatchGroup()
-            for item in items {
-
-                let linkComponents = "\(item.link)".components(separatedBy: "&")
-                assert(linkComponents.count > 0, "Link seems wrong")
-
-                group.enter()
-                client.request(.add(url: item.link))
-                    .sink(receiveCompletion: { completion in
-                        if case let .failure(error) = completion {
-                            print("[Failure] \(item.title)")
-                            print("[Failure] Details: \(error)")
-
-                        }
-                        group.leave()
-                    }, receiveValue: { _ in
-                        print("[Success] \(item.title)")
-                        do {
-                            try self.store.update(item: item, with: .downloaded)
-                        } catch {
-                            print("[Error] Couldn't save new status to DB")
-                        }
-                    })
-                    .store(in: &cancellables)
-            }
-
-
-            let wallTimeout = DispatchWallTime.now() +
-                DispatchTimeInterval.seconds(serverConfig.secondsTimeout ?? 15)
-            let res = group.wait(wallTimeout: wallTimeout)
-            if res == DispatchTimeoutResult.success {
-                print("Tasks successfully submitted")
-            } else {
-                print("Timed out")
-                exit(1)
-            }
+        let clientOpt = getTransmissionClient()
+        guard let client = clientOpt else {
+            print("[ERROR] Failed to connect to transmission client")
+            exit(1)
         }
+
+        var cancellables = Set<AnyCancellable>()
+
+        let items = try store.getPendingDownload()
+        print("[INFO] [\(Date())] Adding \(items.count) new items to transmission")
+
+        let group = DispatchGroup()
+        for item in items {
+
+            let linkComponents = "\(item.link)".components(separatedBy: "&")
+            assert(linkComponents.count > 0, "Link seems wrong")
+
+            group.enter()
+            client.request(.add(url: item.link))
+                .sink(receiveCompletion: { completion in
+                    if case let .failure(error) = completion {
+                        print("[Failure] \(item.title)")
+                        print("[Failure] Details: \(error)")
+
+                    }
+                    group.leave()
+                }, receiveValue: { _ in
+                    print("[Success] \(item.title)")
+                    do {
+                        try self.store.update(item: item, with: .downloaded)
+
+                    } catch {
+                        print("[Error] Couldn't save new status to DB")
+                    }
+                })
+                .store(in: &cancellables)
+        }
+
+
+        let wallTimeout = DispatchWallTime.now() +
+            DispatchTimeInterval.seconds(serverConfig.secondsTimeout ?? 15)
+        let res = group.wait(wallTimeout: wallTimeout)
+        if res == DispatchTimeoutResult.success {
+            print("Tasks successfully submitted")
+        } else {
+            print("Timed out")
+            exit(1)
+        }
+    }
 }
 
 public extension String {
